439c59a13024000d206253c60e4c780b75490154
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-08-23 12:58:16 +0200

    added the codes in the d-counting.pipe.ts to help calculation of days

--- src/app/d-counting.pipe.ts
+++ src/app/d-counting.pipe.ts
@@ -5,8 +5,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DCountingPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(value: any): number {
+    let dayHour:Date = new Date();
+    let day:any =  new Date(dayHour.getFullYear(), dayHour.getMonth(),dayHour.getDate());
+    var sub= Math.abs(day-value);
+    const SInD=86400;
+    var subSec=sub*0.001;
+    var results = subSec/SInD;
+    
+    if (results >= 1 && value > day){
+      return results
+    }else{
+      return 0;
+    }
+    
   }
 
 }

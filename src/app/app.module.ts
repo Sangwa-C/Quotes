5c1da650af7bb8cb61a06fd2d9069b26ae03ee7c
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-08-23 13:12:16 +0200

    added the the quote-form, it will help the user to add her quote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QBodyComponent } from './q-body/q-body.component';
 import { DCountingPipe } from './d-counting.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QBodyComponent,
-    DCountingPipe
+    DCountingPipe,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,

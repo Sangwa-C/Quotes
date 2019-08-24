a4b9f63656e0a101afc306b65eb596c1e2dd588a
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-08-24 19:16:45 +0200

    styled thr for, and other litle things

--- src/app/q-body/q-body.component.ts
+++ src/app/q-body/q-body.component.ts
@@ -9,7 +9,7 @@ export class QBodyComponent implements OnInit {
 
   khaki:Quotes[]=[
 
-    new Quotes("“Don't cry because it's over, smile because it happened.”" , "― Dr. Seuss", new Date(2017,1,14)),
+    // new Quotes("“Don't cry because it's over, smile because it happened.”" , "― Dr. Seuss", new Date(2017,1,14)),
     new Quotes("“Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.”" , "― Bernard M. Baruch", new Date(2017,5,14)),
     new Quotes("“A room without books is like a body without a soul.”" , "― Marcus Tullius Cicero", new Date(2017,12,25)),
     new Quotes("“You've gotta dance like there's nobody watching, Love like you'll never be hurt, Sing like there's nobody listening, And live like it's heaven on earth.”" , "― William W. Purkey", new Date(2018,3,18)),
